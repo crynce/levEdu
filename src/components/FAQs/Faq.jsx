8ddfb2ed39ad8cf98de73c0647bf297390e69f01
crynce <35865352+crynce@users.noreply.github.com>
2022-05-22 05:06:32 +0530

    Update Faq.jsx

--- src/components/FAQs/Faq.jsx
+++ src/components/FAQs/Faq.jsx
@@ -1,60 +1,19 @@
 import React, { useState } from "react";
 import "./Faq.css";
-
-const answerHtml = <div className="answer"> No </div>;
+import FaqQuestions from "./FaqQuestions";
 
 export default function Faq() {
-  const [answer, setAnswer] = useState(false);
-  const faqHandler = () => {
-    setAnswer((prevState) => !prevState);
-  };
   return (
     <div className="faqContainerPrimary">
       <div className="faqContainerSecondary">
         <h2 className="faqHeading">FAQs</h2>
         <div className="faqQuestionContainer">
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer" onClick={faqHandler}>
-              +
-            </span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
-          <div className="faqQuestions">
-            <div className="quesText">How many Students can I refer?</div>
-            <span className="openAnswer">+</span>
-            {answer && answerHtml}
-          </div>
+          <FaqQuestions />
+          <FaqQuestions />
+          <FaqQuestions />
+          <FaqQuestions />
+          <FaqQuestions />
+          <FaqQuestions />
         </div>
         <button className="agreeRefer">Agree & Refer</button>
       </div>
